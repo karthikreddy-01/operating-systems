8e231a9358c090d98b0c7589a9ca802ecd5b4855
karthikreddy-01 <192425157.simats@saveetha.com>
2026-01-22 12:47:07 +0530

    output

--- exp2.c
+++ exp2.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
-int main(int argc, char *argv[]) {
-    int src, dest;
+#include <fcntl.h>
+
+int main()
+{
+    int src_fd, dest_fd;
     char buffer[1024];
     ssize_t bytesRead;
-    if (argc != 3) {
-        printf("Usage: %s <source_file> <destination_file>\n", argv[0]);
-        return 1;
-    }
-    src = open(argv[1], O_RDONLY);
-    if (src < 0) {
-        perror("Error opening source file");
-        return 1;
-    }
-    dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (dest < 0) {
-        perror("Error opening destination file");
-        close(src);
-        return 1;
-    }
-    while ((bytesRead = read(src, buffer, sizeof(buffer))) > 0) {
-        write(dest, buffer, bytesRead);
-    }
-    printf("File copied successfully.\n");
-    close(src);
-    close(dest);
 
+    src_fd = open("source.txt", O_RDONLY);
+    dest_fd = open("destination.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    while ((bytesRead = read(src_fd, buffer, sizeof(buffer))) > 0)
+    {
+        write(dest_fd, buffer, bytesRead);
+    }
+
+    close(src_fd);
+    close(dest_fd);
+
+    printf("File copied successfully.\n");
     return 0;
 }
