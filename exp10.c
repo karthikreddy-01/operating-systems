98e14fa631a7de2de44921895b5ecb19375b0eea
karthikreddy-01 <192425157.simats@saveetha.com>
2026-01-22 13:22:27 +0530

    output

---
+++ exp10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+struct message {
+    long msg_type;
+    char msg_text[100];
+};
+int main() {
+    int msgid;
+    struct message msg;
+    msgid = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
+    if (msgid < 0) {
+        perror("msgget failed");
+        return 1;
+    }
+    if (fork() == 0) {
+        msgrcv(msgid, &msg, sizeof(msg.msg_text), 1, 0);
+        printf("Child Process Received: %s\n", msg.msg_text);
+    } else {
+        msg.msg_type = 1;
+        strcpy(msg.msg_text, "Hello from Parent Process");
+        msgsnd(msgid, &msg, sizeof(msg.msg_text), 0);
+        printf("Parent Process Sent: %s\n", msg.msg_text);
+        wait(NULL);
+        msgctl(msgid, IPC_RMID, NULL);
+    }
+    return 0;
+}
